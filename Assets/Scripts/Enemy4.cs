edf284314a20494d434950967cd0ceee4466e6ea
Yakitori05 <144098931+Yakitori05@users.noreply.github.com>
2024-03-05 16:24:33 +0100

    commit 1
    ik heb gewerkt tot singleton

---
+++ Assets/Scripts/Enemy4.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy4 : MonoBehaviour
+{
+    public float speed = 4f;
+    public float health = 40f;
+    public int points = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public static EnemySpawner Instance;
+
+    public List<GameObject> Path1;
+    public List<GameObject> Path2;
+    public List<GameObject> Enemies;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
